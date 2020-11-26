693426f3886a78d3cebde55f8d71d24c011b84a4
phani kumar kandibanda <phani157@gmail.com>
2020-11-26 10:05:44 -0600

    implemented react life cyccle methods,state and props

--- src/App.js
+++ src/App.js
@@ -5,17 +5,52 @@ import EmployeeDetails from './EmployeeDetails';
 import Grid from './Grid';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    // Don't call this.setState() here!
+    this.counter= 0 ;
+    this.employeeName= 'Phani';
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  // componentWillMount(){
+  //  // alert("componentWillMount");
+  //  }
+
+  componentDidMount(){
+      //alert("componentDidMount");
+  }
+  //componentDidUpdate(prevProps, prevState, snapshot)
+  componentDidUpdate(prevProps, prevState){
+    //alert("componentDidupdate");
+    }
+  
+    // shouldComponentUpdate(nextProps, nextState){
+    //   if(this.counter>4){
+    //     return false;
+    //   }  
+    //   return true;   
+    // }
+
+  handleClick(){
+  this.setState({counter: this.counter++});
+  this.setState(
+    {employeeName : 'Ramesh'} );
+  }
 
   render() {
+    //alert("render");
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <EmployeeDetails data="Passing data from parent"></EmployeeDetails>
-         
+            {this.employeeName}
           <p>
             Edit <code>src/App.js</code> This is for learning.
           </p>
+          {this.counter}
+          <button onClick={this.handleClick}>click on me</button>
           <a
             className="App-link"
             href="https://reactjs.org"
@@ -24,7 +59,7 @@ class App extends React.Component {
           >
             Learn React
           </a> 
-          <Grid tableName="Salary Details" ></Grid>      
+          <Grid tableName="Salary Details  " counter ={this.counter} ></Grid>      
         </header>
       </div>
     );
--- src/Grid.js
+++ src/Grid.js
@@ -3,10 +3,11 @@ import './App.css';
 import React from 'react';
 
 function Grid(props) {
-  // console.log(data);
+  // console.log(data);  
     return (
       <div className="App">
           {props.tableName}
+          {props.counter}
         <table>
             <tr><td>column1</td><td>column2</td></tr>
             <tr><td>column1</td><td>column2</td></tr>
